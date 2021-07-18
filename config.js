29955b9e66cb014be4db8d9d4c1eed189897cebd
Lewis Potticary <lewispotticary@gmail.com>
2021-07-18 11:04:19 +0100

    Config file where API keys are stored

---
+++ config.js
@@ -0,0 +1,4 @@
+var config = {
+    GEOCODING_API_KEY : '', //Enter your geocoding API key from; https://developers.google.com/maps/documentation/geocoding/get-api-key
+    METOFFICE_API_KEY : '', //Enter you metoffice API key from; https://www.metoffice.gov.uk/services/data/datapoint/api
+}
